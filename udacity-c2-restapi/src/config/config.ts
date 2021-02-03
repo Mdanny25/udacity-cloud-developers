ad0e1b209ad7815d57de98c9c061e8a003821ba7
Mdanny25 <dannymugisha25@gmail.com>
2021-02-03 13:16:03 +0200

    working signedUrl

--- udacity-c2-restapi/src/config/config.ts
+++ udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
-  "dev": {
-    "username": "postgres",
-    "password": "rdschallengedme",
-    "database": "md_database",
-    "host": "rds-posgresql-exercise-md.cx9rlx1wfnfq.us-east-2.rds.amazonaws.com",
+    "dev": {
+        "username": "postgres",
+        "password": "rdschallengedme",
+        "database": "md_database",
+        "host": "rds-posgresql-exercise-md.cx9rlx1wfnfq.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-mugisha-dev"
   },
   "prod": {
     "username": "",
